c0dc3159fac4da3670fee7906729833c30851e7e
Shegspy <youremail>
2024-07-24 02:59:25 -0700

    Map Data Structure

--- script.js
+++ script.js
@@ -52,6 +52,78 @@ const restaurant = {
   },
 };
 
+// Maps advance
+const question = new Map([
+  ['question', 'Whats the best programming language'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct'],
+  [false, 'Try again'],
+]);
+
+console.log(question);
+
+// convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+
+console.log(hoursMap);
+
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+const answer = Number(prompt('Your anwser:'));
+console.log(answer);
+
+// answer === 3
+//   ? console.log(question.get(true))
+//   : console.log(question.get(false));
+
+console.log(question.get(question.get('correct') === answer));
+
+// convert to Array
+console.log([...question]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+/*
+// Maps: Fundamental 
+
+const rest = new Map();
+rest.set('name', 'Emmanuel Babalola');
+rest.set(1, 'Yola Adamawa');
+rest.set(2, 'Ikotun Lagos');
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'we are closed :(');
+
+console.log(rest);
+console.log(rest.get('categories'));
+console.log(rest.get('name'));
+console.log(rest.get(2));
+
+const time = 8;
+console.log(rest.get(time > rest.set('open') && time < rest.set('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
 
 
 const orderSet = new Set([
@@ -531,3 +603,5 @@ for (const [i, j] of Object.entries(game.odds)) {
 }
 
 //*/
+
+//
